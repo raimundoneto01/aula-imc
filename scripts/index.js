f8115c842b14093f9156623e793994f6c160cb1f
raimundoneto01 <raimundogomes826@gmail.com>
2022-11-09 20:23:56 -0300

    atualizando

--- scripts/index.js
+++ scripts/index.js
@@ -2,14 +2,23 @@ function calcularIMC() {
     let mensagem = document.getElementById("mensagem");
     let altura = parseFloat(document.getElementById("altura").value);
     let peso = parseFloat(document.getElementById("peso").value);
-    //let imc = peso / (altura * altura);
+    let imc = peso / (altura * altura);
          let imc = 18.6;
+         
         if(imc < 18.5) {
             mensagem.innerText = "baixo peso";
 
         } else if (imc >= 18.5 && imc <= 24.9) {
             mensagem.innerText = "peso Normal";
-        } else {
+        } else if (imc >= 25 && imc <= 29.9) {
+            mensagem.innerText = "Sobrepeso";
+        }else if (imc >= 30 && imc <= 34.9) {
+            mensagem.innerText = "Obesidade Grau 1";
+        } else if (imc >= 35 && imc <= 39.9) {
+            mesagem.innerText = "Obesidade Grau 2";
+        } else if (imc >= 40) {
+            mensagem.innerText = "Obesidade Grau 3 'MOrbida'";
+        }
 
         }
 }
